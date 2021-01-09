a4b572bd93d793685f4e343aeb91b2ec28118918
Ivan <ivan.martins10@gmail.com>
2021-01-09 12:21:21 -0300

    nova etapa do curso

--- routes/admin.js
+++ routes/admin.js
@@ -2,5 +2,48 @@ var express = require("express")
 var router = express.Router()
 
 router.get('/', (req, res, next) => {
-  
+
+  res.render('admin/index')
+
 })
+
+router.get('/login', (req, res, next) => {
+
+  res.render('admin/login')
+
+})
+
+router.get('/contacts', (req, res, next) => {
+
+  res.render('admin/contacts')
+
+})
+
+router.get('/emails', (req, res, next) => {
+
+  res.render('admin/emails')
+
+})
+
+router.get('/menus', (req, res, next) => {
+
+  res.render('admin/menus')
+
+})
+
+router.get('/reservations', (req, res, next) => {
+
+  res.render('admin/reservations', {
+    date: {}
+  })
+
+})
+
+router.get('/users', (req, res, next) => {
+
+  res.render('admin/users')
+
+})
+
+
+module.exports = router;
